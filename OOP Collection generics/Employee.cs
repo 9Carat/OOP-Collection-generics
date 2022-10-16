e22005a8d6e5b1179be8f0273d60ae5585f91f73
Christoffer Ottestig <otte365@hotmail.com>
2022-10-16 13:29:43 +0200

    Add project files.

---
+++ OOP Collection generics/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Collection_generics
+{
+    public class Employee
+    {
+        public int ID;
+        public string Name;
+        public string Gender;
+        public int Salary;
+
+        public Employee(int ID, string Name, string Gender, int Salary)
+        {
+            this.ID = ID;
+            this.Name = Name;
+            this.Gender = Gender;
+            this.Salary = Salary;
+        }
+    }
+}
---
+++ OOP Collection generics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+
+namespace OOP_Collection_generics
+{
+    internal class Program
+    {
+        //public static void Display<T>(Stack)
+        //{
+        //    foreach (T item in stack)
+        //    {
+        //        Console.Write(item + " ");
+        //    }
+        //    Console.WriteLine();
+        //}
+
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee(101, "Alex", "Male", 45000);
+            Employee emp2 = new Employee(102, "Antonia", "Female", 55000);
+            Employee emp3 = new Employee(103, "Amanda", "Female", 35000);
+            Employee emp4 = new Employee(104, "Oskar", "Male", 25000);
+            Employee emp5 = new Employee(105, "Stefan", "Male", 350000);
+
+            Stack<object> stack = new Stack<object>();
+
+            stack.Push(emp1);
+            stack.Push(emp2);
+            stack.Push(emp3);
+            stack.Push(emp4);
+            stack.Push(emp5);
+
+            Console.WriteLine(stack.Peek());
+            
+           
+        }
+    }
+}
